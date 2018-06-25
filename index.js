80648518c9b04e3756957d0133cce25f901ab7e5
Abel Enrique Cruz <abelenriquecruz@gmail.com>
2018-06-25 20:44:40 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+function add() {
+  a + b
+}
+
+function subtract(a, b) {
+  a - b
+}
+
+function multiply(a, b) {
+  a * b
+}
+
+function divide(a, b) {
+  a / b
+}
+
+function inc(n) {
+  var number = n
+  number++
+  number
+}
